40cddc5b36de98f3219efe4e87c27ca52998631f
jtsmith9562 <jtsmith9562@gmail.com>
2021-11-04 09:45:43 -0400

    saving

--- MySQLHeroAPI.php
+++ MySQLHeroAPI.php
@@ -14,137 +14,141 @@ if ($conn->connect_error) {
 }
 
 
-echo $_POST;
+// echo $_POST;
 //todo: send the post object instead of each item
 
-function createHero($_REQUEST)
+function createHero()
 {
-    $name = $_POST["name"];
-    $about_me = $_POST["about_me"];
-    $biography = $_POST["biography"];
 
-    if (!isSet($_GET["name"])){
+
+    if (!isset($_REQUEST["name"])) {
         echo "ERROR 422: undefined variable, expected 'name'";
         return;
     }
-    if (!isSet($_GET["about_me"])){
+    if (!isset($_REQUEST["about_me"])) {
         echo "ERROR 422: undefined variable, expected 'about_me'";
         return;
     }
-     if (!isSet($_GET["biography"])){
-         echo "ERROR 422: undefined variable, expected 'biography'";
-         return;
-    } else {   
-        "INSERT INTO heroes (name, about_me, biography) VALUES ('$name', '$about_me', '$biography')";
+    if (!isset($_REQUEST["biography"])) {
+        echo "ERROR 422: undefined variable, expected 'biography'";
+        return;
     }
-    
+    $name = $_REQUEST["name"];
+    $about_me = $_REQUEST["about_me"];
+    $biography = $_REQUEST["biography"];
+    $sql =  "INSERT INTO heroes (name, about_me, biography) VALUES ('$name', '$about_me', '$biography')";
+    global $conn;
+    if ($conn->query($sql) === true) {
+        echo 'success';
+    } else {
+        echo $conn->error;
+    }
 }
 
 
-// function updateHero($id, $name, $about_me, $biography){
-    //     //
-    //     //array_splice($_SESSION["heroes"],$index,1,[[$name, $tagline]]);
-    //     $servername = "localhost";
-    //     $username = "root";
-//     $password = "";
-//     $dbname = "Hero_Database";
-
-//     // Create connection
-//     $conn = new mysqli($servername, $username, $password, $dbname);
-//     // Check connection
-//     if ($conn->connect_error) {
-    //     die("Connection failed: " . $conn->connect_error);
-    //     }
-
-    //     $sql = "UPDATE heroes SET name='$name', about_me='$about_me', biography='$biography' WHERE id=$id";
-    
-//     if ($conn->query($sql) === TRUE) {
-    //     echo "Record updated successfully";
-    //     } else {
-        //     echo "Error updating record: " . $conn->error;
-        //     }
-        //     $conn->close();
-        
-        // }
-        
-        // function deleteHero($id){
-            //     $servername = "localhost";
-            //     $username = "root";
-            //     $password = "";
-            //     $dbname = "Hero_Database";
-            
-            //     // Create connection
-            //     $conn = new mysqli($servername, $username, $password, $dbname);
-            //     // Check connection
-            //     if ($conn->connect_error) {
-                //     die("Connection failed: " . $conn->connect_error);
-                //     }
-                
-                //     // sql to delete a record
-                //     $sql = "DELETE FROM heroes WHERE id=$id";
-                
-                //     if ($conn->query($sql) === TRUE) {
-                    //     echo "Record deleted successfully";
-                    //     } else {
-                        //     echo "Error deleting record: " . $conn->error;
-                        //     }
-                        // }
-                        
-
-                        
-                        
-                        
-                        
-                        
-
-                        // function getAllHeroes(){
-                            //     // get the heroes, return the heroes
-                            
-                            //     try {
-//         $this->connect();
-//         $sql = "SELECT * FROM heroes";
-//         $result = $this->conn->query($sql);
-
-//         if ($result->num_rows > 0) {
-    //         // output data of each row
-    //             while($row = $result->fetch_assoc()) {
-        //                 // create an object 
-        //                 // append it to the heroes array
-        //                 $heroObj = new HeroObject($row["nickname"], $row["tagline"]);
-        //                 array_push($this->heroes, $heroObj);
-        //             }
-        //         } else {
-//             $this->heroes = [];
-//         }
-//         $this->disconnect();
-//     }
-//     //catch exception
-//     catch(Exception $e) {
-    //         echo 'Message: ' .$e->getMessage();
-    //     }
-    //     return $this->heroes;
-    // }
-if (isSet($_GET['route'])) {
- 
-    switch ($_GET['route']) {
-        case "create":
-        createHero($_POST);
-        break;
-        case "read":
-        //readAllHeroes();
-        break;
-        case "update":
-        //updateHero($_GET);
-        break;
-        case "delete":
-        deleteHero($_GET);
-        break;
-        default:
-        echo 'ERROR 404, page not found.';
+function updateHero()
+{
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "Hero_Database";
+    // change to if name,about,bio exists, add to string variable, set sql = string
+    if (!isset($_REQUEST["name"])) {
+        echo "ERROR 422: undefined variable, expected 'name'";
+        return;
     }
-} else {
-    echo 'Welcome to the Homepage';
+    if (!isset($_REQUEST["about_me"])) {
+        echo "ERROR 422: undefined variable, expected 'about_me'";
+        return;
+    }
+    if (!isset($_REQUEST["biography"])) {
+        echo "ERROR 422: undefined variable, expected 'biography'";
+        return;
+    }
+    $name = $_REQUEST["name"];
+    $about_me = $_REQUEST["about_me"];
+    $biography = $_REQUEST["biography"];
+    $id = $_REQUEST["id"];
+
+    $sql = "UPDATE heroes SET name='$name', about_me='$about_me', biography='$biography' WHERE id=$id";
+    global $conn;
+    if ($conn->query($sql) === TRUE) {
+        echo "Record updated successfully";
+    } else {
+        echo "Error updating record: " . $conn->error;
+    }
 }
-    
-    $conn->close();
-    
+
+function deleteHero()
+{
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "Hero_Database";
+
+    if (!isset($_REQUEST["id"])) {
+        echo "ERROR 422: undefined variable, expected 'id'";
+        return;
+    }
+    // sql to delete a record
+    $id = $_REQUEST["id"];
+    $sql = "DELETE FROM heroes WHERE id=$id";
+    global $conn;
+    if ($conn->query($sql) === TRUE) {
+        echo "Record deleted successfully";
+    } else {
+        echo "Error deleting record: " . $conn->error;
+    }
+}
+
+function getAllHeroes()
+{
+    // get the heroes, return the heroes
+
+    $sql = "SELECT 
+    heroes.name, 
+    heroes.about_me, 
+    heroes.biography,  
+    GROUP_CONCAT( ability_type.ability SEPARATOR ', ') as abilities 
+    FROM heroes 
+    INNER JOIN abilities ON abilities.hero_id=heroes.id
+    INNER JOIN ability_type ON ability_type.id=abilities.ability_id
+    GROUP BY heroes.id";
+    global $conn;
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+        // output data of each row
+        while ($row = $result->fetch_assoc()) {
+           print_r($row);
+        }
+    } else {
+        echo "0 results";
+    }
+    //catch exception
+}
+    //switch case
+    if (isset($_GET['route'])) {
+
+        switch ($_GET['route']) {
+            case "create":
+                createHero();
+                break;
+            case "read":
+                getAllHeroes();
+                break;
+            case "update":
+                updateHero();
+                break;
+            case "delete":
+                deleteHero();
+                break;
+            default:
+                echo 'ERROR 404, page not found.';
+                break;
+        }
+    } else {
+        echo 'Welcome to the Homepage';
+    }
+
+$conn->close();
